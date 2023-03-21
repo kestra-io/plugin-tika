444bc5eecce7241a25da68e176717caac2c5e4eb
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:40:11 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/tika/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Apache Tika.\n" +
+        "The Apache Tika toolkit detects and extracts metadata and text from over a thousand different file types (such as PPT, XLS, and PDF).",
+    categories = PluginSubGroup.PluginCategory.TRANSFORMATION
+)
+package io.kestra.plugin.tika;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
